b7eed943732bda424327c48b8e5beb7f7aed0dd2
reichelz3777 <98786595+reichelz3777@users.noreply.github.com>
2022-02-14 20:44:45 -0500

    Add files via upload

---
+++ P1HW1_ReichelZachary.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter the first number: "))
+
+num2 = int(input("Enter the second number: "))
+
+sum = num1+num2
+
+product = num1*num2
+
+print('First Number Entered ',num1)
+print(num2)
+print(sum)
+print(product)

99c0cae82ce0cef3b3e0d4a101547d19351cb86f
reichelz3777 <98786595+reichelz3777@users.noreply.github.com>
2022-02-16 17:55:54 -0500

    Add files via upload

---
+++ P1HW2_ReichelZachary.py
@@ -0,0 +1,16 @@
+# Pizza Order
+# 2-16-22
+# CTI-110 P1HW2 - Pizza Order
+# Zachary Reichel
+#
+
+numstudents = int(input("How many students do you want to order pizza?"))
+numslices = int(numstudents*3)
+numpizzas = int(numstudents/2)
+
+print("------Pizza Order------")
+print("Number of Students: ",numstudents)
+print("Pizza Slices Needed: ",numslices)
+print("Pizzas Needed: ",numpizzas)
+print("---------------------")
+

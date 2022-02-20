fba7289770d4e557bc280fe1c2618ed3ef9752c2
reichelz3777 <98786595+reichelz3777@users.noreply.github.com>
2022-02-20 15:44:58 -0500

    Add files via upload

---
+++ P2LAB1_ReichelZachary.py
@@ -0,0 +1,7 @@
+user_int=int(input("Enter integer (32 - 126):\n"))
+user_float=float(input("Enter float:\n")) 
+user_character=input("Enter character:\n") 
+user_string=input("Enter string:\n") 
+print(user_int,user_float,user_character,user_string)
+print(user_string,user_character,user_float,user_int)
+print(user_int,"converted to a character is",chr(user_int))
